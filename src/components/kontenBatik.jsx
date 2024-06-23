2e8cf3f756bbd06fca80d5fd4ca028217cd063b1
virgoridwan <virgonita62@gmail.com>
2024-06-23 23:53:59 +0700

    update2

--- src/components/kontenBatik.jsx
+++ src/components/kontenBatik.jsx
@@ -3,7 +3,6 @@ import { fetchData } from '../api';
 
 function KontenMembatik() {
   const [kontenMembatik, setKontenMembatik] = useState([]);
-  const [currentVideo, setCurrentVideo] = useState(null);
 
   useEffect(() => {
     const fetchKontenMembatik = async () => {
@@ -18,28 +17,6 @@ function KontenMembatik() {
     fetchKontenMembatik();
   }, []);
 
-  const playVideo = (id) => {
-    if (currentVideo) {
-      currentVideo.pause(); 
-    }
-    const video = document.getElementById(`video-${id}`);
-    setCurrentVideo(video);
-    video.play();
-  };
-
-  useEffect(() => {
-    const handleVideoEnd = () => {
-      setCurrentVideo(null); 
-    };
-
-    if (currentVideo) {
-      currentVideo.addEventListener('ended', handleVideoEnd);
-      return () => {
-        currentVideo.removeEventListener('ended', handleVideoEnd);
-      };
-    }
-  }, [currentVideo]);
-
   return (
     <section id="konten" className="mt-0 px-4 py-20 bg-white shadow-md relative">
       <div className="konten-container">
@@ -53,11 +30,12 @@ function KontenMembatik() {
                 <h2 className="konten-name text-3xl mb-4">{konten.title}</h2>
                 <p className="konten-description">{konten.description}</p>
               </div>
-              <div className="video-container mt-4">
-                <video id={`video-${konten.id}`} controls className="w-full">
-                  <source src={`http://localhost:5000${konten.src}`} type="video/mp4" />
-                  Browser Anda tidak mendukung tag video atau format file video ini.
-                </video>
+              <div className="image-container mt-4">
+                <img 
+                  src={`http://localhost:5000${konten.src}`} 
+                  alt={konten.title} 
+                  className="w-full rounded-lg" 
+                />
               </div>
               <p className="text-xs mt-2">Sumber: <a href={konten.sourceUrl} target="_blank" rel="noopener noreferrer" className="underline">{konten.sourceUrl}</a></p>
             </div>
